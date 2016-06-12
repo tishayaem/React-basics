d42757a1c7efbda897e7a7c85e91b077f5a791f5
Constantin <kostiantynkalinin@gmail.com>
2016-06-12 22:53:00 +0100

    Created components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>;
+);
 }
+
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
