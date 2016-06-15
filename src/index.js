92aabefbb1dfe2f3415a9518200ab65dbfcc03d4
Constantin <kostiantynkalinin@gmail.com>
2016-06-15 23:59:26 +0100

    Youtube API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const App = () => {
   return (
