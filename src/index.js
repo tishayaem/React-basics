942cc2a23a9d20977327de335fc2a7f8a69e1b54
Constantin <kostiantynkalinin@gmail.com>
2016-06-16 23:46:09 +0100

    Refactored App component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+var API_KEY = process.env.API_TOKEN;
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-);
-};
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+  YTSearch({key: API_KEY, term: 'pianocat'}, (data) => {
+    this.setState({ videos: data});
+  });
+}
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
